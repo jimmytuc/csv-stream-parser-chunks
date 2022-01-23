5797746b9a3881f15c84a3ccba0b2b562f78627f
Phuc <jimmytucsolo@gmail.com>
2022-01-23 15:10:26 +0700

    add instruction

--- test.js
+++ test.js
@@ -4,6 +4,8 @@ const CSVFastParser = require('./parser')
 const parser = new CSVFastParser(path.resolve(__dirname, './source/transactions.csv'), 100)
 
 parser.read((data) => {
-    console.log(data)
+    // console.log(data)
+    // parsing data here
+    // keep reading until the end of file
     parser.continue()
 })
